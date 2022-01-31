a4b0ca0141b77b2a23125f182af44acc14fe4f48
Valentin Trinqué <4662842+ValentinTrinque@users.noreply.github.com>
2022-01-31 15:32:32 +0000

    vegawallet: add 0.11.0 bottle.

--- Formula/vegawallet.rb
+++ Formula/vegawallet.rb
@@ -6,6 +6,12 @@ class Vegawallet < Formula
   license "MIT"
   head "https://github.com/vegaprotocol/vegawallet.git", branch: "develop"
 
+  bottle do
+    root_url "https://github.com/vegaprotocol/homebrew-vega/releases/download/vegawallet-0.11.0"
+    sha256 cellar: :any_skip_relocation, big_sur:      "1317731fb5f9fc45095b08f6b789941d97db1359d6cab02df623633291f08b13"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "ab91168417b7bdda8fa70a2aacdeb5493a98de4d1c2e790b70f1359105f5c02e"
+  end
+
   depends_on "go" => :build
 
   def install
