41427d7cf57fd32a5c8b8f5db4d70ff113a35955
Valentin Trinque <val.trinque@gmail.com>
2022-01-31 22:04:38 +0100

    Revert build of arm64 bottles
    Closes #4.
    
    Signed-off-by: Jeremy Letang <me@jeremyletang.com>
    Signed-off-by: Valentin Trinqué <4662842+ValentinTrinque@users.noreply.github.com>

--- Formula/vegawallet.rb
+++ Formula/vegawallet.rb
@@ -4,10 +4,10 @@ class Vegawallet < Formula
   url "https://github.com/vegaprotocol/vegawallet/archive/refs/tags/v0.11.0.tar.gz"
   sha256 "c928373d6a84575107d235525c2f7f98bf2e6cb84f75e7a51de66a02189a35ff"
   license "MIT"
-  revision 1
   head "https://github.com/vegaprotocol/vegawallet.git", branch: "develop"
 
   bottle do
+    rebuild 1
     root_url "https://github.com/vegaprotocol/homebrew-vega/releases/download/vegawallet-0.11.0_1"
     sha256 cellar: :any_skip_relocation, big_sur:      "75de666ca55e788d32699373ec2569183a231a4a9a32d478b12a5197ebe3891d"
     sha256 cellar: :any_skip_relocation, x86_64_linux: "777e31ba910d0eb621088001bbb6dfb3fb81681eabb95fb26170cdbf03220b26"
