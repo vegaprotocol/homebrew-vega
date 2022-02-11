334d8f0d10fc7f4a978cfa0a6a63dfbb95b282c9
Valentin Trinque <val.trinque@gmail.com>
2022-02-11 11:38:11 +0100

    vegawallet 0.12.0
    Update vegawallet to 0.12.0
    
    Closes #8.
    
    Signed-off-by: Valentin Trinqué <4662842+ValentinTrinque@users.noreply.github.com>

--- Formula/vegawallet.rb
+++ Formula/vegawallet.rb
@@ -1,8 +1,8 @@
 class Vegawallet < Formula
   desc "Command-line tool to manage your Vega Protocol wallet"
-  homepage "https://docs.vega.xyz/docs/tools/vega-wallet/cli-wallet/latest/create-wallet"
-  url "https://github.com/vegaprotocol/vegawallet/archive/refs/tags/v0.11.0.tar.gz"
-  sha256 "c928373d6a84575107d235525c2f7f98bf2e6cb84f75e7a51de66a02189a35ff"
+  homepage "https://docs.vega.xyz/docs/tools/vega-wallet/cli-wallet"
+  url "https://github.com/vegaprotocol/vegawallet/archive/refs/tags/v0.12.0.tar.gz"
+  sha256 "0aaaec28be20aa2ed749d896434a5b2cd52d0013d718725e7a66acff74eef577"
   license "MIT"
   head "https://github.com/vegaprotocol/vegawallet.git", branch: "develop"
 
