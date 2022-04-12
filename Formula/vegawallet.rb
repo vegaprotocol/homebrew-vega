291317690216b9df536c09f6d1b2f1b2ea8805c8
Valentin Trinque <val.trinque@gmail.com>
2022-04-12 17:22:18 +0200

    vegawallet 0.14.0
    Closes #11.
    
    Signed-off-by: Valentin Trinqué <4662842+ValentinTrinque@users.noreply.github.com>

--- Formula/vegawallet.rb
+++ Formula/vegawallet.rb
@@ -1,13 +1,13 @@
 class Vegawallet < Formula
   desc "Command-line tool to manage your Vega Protocol wallet"
   homepage "https://docs.vega.xyz/docs/tools/vega-wallet/cli-wallet"
-  url "https://github.com/vegaprotocol/vegawallet/archive/refs/tags/v0.13.1.tar.gz"
-  sha256 "e6d95dfdfdbd3b145c3fbda0cc71dcb1beb70281d9f985d04682d92374e48f88"
+  url "https://github.com/vegaprotocol/vegawallet/archive/refs/tags/v0.14.0.tar.gz"
+  sha256 "bc6ae1706600f6bb241f164fd0169b0e8fcd5cfad9525814f9616326c5b66eb3"
   license "MIT"
   head "https://github.com/vegaprotocol/vegawallet.git", branch: "develop"
 
   bottle do
-    root_url "https://github.com/vegaprotocol/homebrew-vega/releases/download/vegawallet-0.13.1"
+    root_url "https://github.com/vegaprotocol/homebrew-vega/releases/download/vegawallet-0.13.2"
     sha256 cellar: :any_skip_relocation, big_sur:      "9316af5dbbdfdef5462c28019ec32bfdc430021c22075a9bc68a8b82c5d4369c"
     sha256 cellar: :any_skip_relocation, x86_64_linux: "4f7454722f9bd77e9b330d63df859c37b9d2c9ad6b5524be16034751a60274f8"
   end
