d5411d2046237f03f00c09a21cef1d5d07b8dac7
Valentin Trinqué <4662842+ValentinTrinque@users.noreply.github.com>
2023-10-23 09:33:22 +0000

    visor: add 0.71.4 bottle.

--- Formula/visor.rb
+++ Formula/visor.rb
@@ -5,6 +5,12 @@ class Visor < Formula
   license all_of: ["MIT", "BSL-1.0"]
   head "https://github.com/vegaprotocol/vega.git", branch: "develop"
 
+  bottle do
+    root_url "https://github.com/vegaprotocol/homebrew-vega/releases/download/visor-0.71.4"
+    sha256 cellar: :any_skip_relocation, monterey:     "a3f302a2c0d3aea9506c2f09aa40bba56dcd2fae1eec47e6def77045f9794e64"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "48e87f62c4d473957d2cf2ef85cc764c14f79abf53e72af6597bec250d1d8afb"
+  end
+
   depends_on "go@1.19" => :build
 
   def install
