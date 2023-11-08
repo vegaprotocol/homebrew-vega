0abb987250bfdcf09bcae445beaf13c78ab58854
Valentin Trinqué <4662842+ValentinTrinque@users.noreply.github.com>
2023-11-08 13:00:26 +0000

    vega: add 0.73.3 bottle.

--- Formula/vega.rb
+++ Formula/vega.rb
@@ -5,6 +5,12 @@ class Vega < Formula
   license all_of: ["MIT", "BSL-1.0"]
   head "https://github.com/vegaprotocol/vega.git", branch: "develop"
 
+  bottle do
+    root_url "https://github.com/vegaprotocol/homebrew-vega/releases/download/vega-0.73.3"
+    sha256 cellar: :any_skip_relocation, monterey:     "cd074b7af23194ccc7111b40a4c68fbc2b079b9adc5483eeeb17112f9b35079e"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "5571127a320cfb7da07755c327f0862a92ed858bb554e87e016b7c62b89cb205"
+  end
+
   depends_on "go@1.20" => :build
 
   def install
