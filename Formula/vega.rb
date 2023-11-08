83b6eaea5da05a1230b501e3b486b2e9c3ef243e
Valentin Trinque <val.trinque@gmail.com>
2023-11-08 11:15:17 +0100

    release: Vega 0.73.3

--- Formula/vega.rb
+++ Formula/vega.rb
@@ -1,14 +1,9 @@
 class Vega < Formula
   desc "Command-line tool that manages your Vega Protocol node"
   homepage "https://docs.vega.xyz/mainnet/node-operators"
-  url "https://github.com/vegaprotocol/vega.git", tag: "v0.72.14", revision: "282fe5a94609406fd638cc1087664bfacb8011bf"
+  url "https://github.com/vegaprotocol/vega.git", tag: "v0.73.3", revision: "f76796a4a8c1f60650f35cf907a7d6ac492d7f97"
   license all_of: ["MIT", "BSL-1.0"]
   head "https://github.com/vegaprotocol/vega.git", branch: "develop"
-
-  bottle do
-    root_url "https://github.com/vegaprotocol/homebrew-vega/releases/download/vega-0.72.14"
-    sha256 cellar: :any_skip_relocation, monterey: "b4e66edca3f8e00a5c058b6c0e6a7da4231da88a9658e94cc14e1fc816db5a66"
-  end
 
   depends_on "go@1.20" => :build
 
--- Formula/visor.rb
+++ Formula/visor.rb
@@ -1,15 +1,9 @@
 class Visor < Formula
   desc "Command-line tool that manages Vega Protocol upgrades of nodes"
   homepage "https://docs.vega.xyz/mainnet/node-operators/get-started/setup-validator#configure-visor"
-  url "https://github.com/vegaprotocol/vega.git", tag: "v0.72.14", revision: "282fe5a94609406fd638cc1087664bfacb8011bf"
+  url "https://github.com/vegaprotocol/vega.git", tag: "v0.73.3", revision: "f76796a4a8c1f60650f35cf907a7d6ac492d7f97"
   license all_of: ["MIT", "BSL-1.0"]
   head "https://github.com/vegaprotocol/vega.git", branch: "develop"
-
-  bottle do
-    root_url "https://github.com/vegaprotocol/homebrew-vega/releases/download/visor-0.72.14"
-    sha256 cellar: :any_skip_relocation, monterey:     "a5affb4af98ebaecbd09c1f28fc434ae6a4a18ec76cdf73e945229eb5b509601"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "ebc6e2fc9f499f92303b688f3d003417cc418444756db2506f5e8b2d7e355764"
-  end
 
   depends_on "go@1.20" => :build
 
