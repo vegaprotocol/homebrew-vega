728dbf8f44494d72416781aab928683cdb388356
Valentin Trinqué <4662842+ValentinTrinque@users.noreply.github.com>
2023-11-20 14:32:39 +0000

    vegawallet: add 0.73.5 bottle.

--- Formula/vegawallet.rb
+++ Formula/vegawallet.rb
@@ -5,6 +5,12 @@ class Vegawallet < Formula
   license all_of: ["MIT", "BSL-1.0"]
   head "https://github.com/vegaprotocol/vega.git", branch: "develop"
 
+  bottle do
+    root_url "https://github.com/vegaprotocol/homebrew-vega/releases/download/vegawallet-0.73.5"
+    sha256 cellar: :any_skip_relocation, monterey:     "269f6489a08dc34d6c0a1e944d61f2101f183179b727c9d9af3659e4da526ea1"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "e5c0a7c4a88a29b40e3a9f8d7a979a97dbdcffd6e5703333a39d6e128ed1d2d0"
+  end
+
   depends_on "go@1.20" => :build
 
   def install
